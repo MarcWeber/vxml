50a54c8015932c74a261ce43c15f4ef4d1ac9bc7
Marc Weber <marco-oweber@gmx.de>
2008-08-29 14:33:56 +0200

    using << <<< in test.hs now

--- test/test.hs
+++ test/test.hs
@@ -1,91 +1,43 @@
 {-# OPTIONS_GHC -fcontext-stack=500 #-}
 {-# OPTIONS_GHC -XTemplateHaskell -XStandaloneDeriving -XTypeSynonymInstances -XMultiParamTypeClasses -XFunctionalDependencies #-}
-{-# LANGUAGE NoMonomorphismRestriction,  OverlappingInstances, UndecidableInstances,  FlexibleContexts,  FlexibleContexts,  FlexibleInstances,  EmptyDataDecls #-}
+{-# LANGUAGE FlexibleInstances,  FlexibleInstances,  FlexibleInstances,  NoMonomorphismRestriction,  UndecidableInstances,  FlexibleContexts,  EmptyDataDecls #-}
+
+{- see cabal file
+#ifndef TypeToNat
+  {-# Language OverlappingInstances  #-}
+#endif
+-}
+
 module Main where
-import Language.Haskell.TH
+import Prelude hiding (head,div)
 import Control.Monad
-import Directory
-import Control.Exception
 import System.FilePath
 import System.IO
 import System.Environment
 import Text.XML.Validated.Types
 import Text.XML.Validated.Instance.String
-import Text.XML.HaXml.Parse
-import qualified Data.Map as M
-import Data.Maybe
+import Text.XML.HaXml.Types
+
 import Text.XML.HaXml.Types
 import Text.XML.Validated.TH
 
--- deriving instance Show DocTypeDecl
--- deriving instance Show MarkupDecl
-
--- deriving instance Show ElementDecl
--- deriving instance Show AttListDecl
--- deriving instance Show EntityDecl
--- deriving instance Show NotationDecl
--- deriving instance Show Misc
-
--- deriving instance Show PublicID
--- deriving instance Show GEDecl
--- deriving instance Show PEDecl
--- deriving instance Show AttDef
--- deriving instance Show ContentSpec
-
--- deriving instance Show Mixed
--- deriving instance Show DefaultDecl
--- deriving instance Show AttType
--- deriving instance Show EntityDef
-
--- deriving instance Show NDataDecl
--- deriving instance Show TokenizedType
--- deriving instance Show EnumeratedType
--- deriving instance Show FIXED
-
-
-withCurrentDirectory :: FilePath -> IO a -> IO a
-withCurrentDirectory path f = do
-    cur <- getCurrentDirectory
-    setCurrentDirectory path
-    finally f (setCurrentDirectory cur)
-
--- hack, change current working directory cause HaXmL doesn't find ENTITY refs 
-readDTD file = do
-                print =<< getCurrentDirectory
-                liftM (dtdParse' file) $ readFile file
-
--- HaXmL only returns a loose list of element and attr list declarations 
--- this functions ties them.
+-- import TestXHTML
 
 $( 
- dtdToTypes "dtds/xhtml1-20020801/DTD/xhtml1-strict_onefile.dtd" 
- --   dtdToTypes "dtds/test_simple.dtd" 
+      dtdToTypes "dtds/xhtml1-20020801/DTD/xhtml1-strict_onefile.dtd" 
+      -- dtdToTypes "dtds/test_simple.dtd" 
               (XmlIds (Just "-//W3C//DTD XHTML 1.0 Strict//EN") 
                       (Just "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd") ) 
  )
 
-
-
 (<<) = addElT
+(<<<) = addTextT
 
---  )
 main = do
-  -- [arg] <- getArgs
-  -- readdtd arg >>= \r -> case r of
-    -- left a -> putstrln a
-    -- right (just (dtd name mbextid decls))  -> mapm_ print $ zipelements decls
-  -- putStrLn $ xmlDocT $ addAttrT (addTextT (addElT (addElT root b) a) "abc") (undefined :: Id_A) "test"
-  -- putStrLn $ xmlDocT $  root `id_A` "test"
-  -- putStrLn $ xmlDocT $ addElT ( root `id_A` "test")
-  --                              a
-  putStrLn $ xmlDocT $ (addElT html  ( Main.head `addElT` (title) ) )
-                          `addElT` ( body )
 
-
-  -- putStrLn (show (undefined :: (HEq Id_A Id_A a) => a ))
-  -- putStrLn (show (undefined :: (TypeToNat A_T a) => a ))
-  -- putStrLn (show (undefined :: (TypeToNat B_T a) => a ))
-  -- putStrLn $ fromPT $ endElT $  createElT (undefined :: Root_T)
+  putStrLn $ xmlDocT $
+    ((html << (head << (title <<< "hw")))
+          <<  (body << (div <<< "test" )) )
   print "end"
 
 
